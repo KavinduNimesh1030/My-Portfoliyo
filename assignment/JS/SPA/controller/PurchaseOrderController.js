0eda9ff84cda47476702ae743b18beea3b43753d
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2022-10-13 02:19:51 +0530

    disabled previous date in the date picker

--- assignment/JS/SPA/controller/PurchaseOrderController.js
+++ assignment/JS/SPA/controller/PurchaseOrderController.js
@@ -8,8 +8,21 @@ var t = 0;
 
 $(document).ready(function () {
     displayOrderId();
-
+    a();
 });
+
+function a(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+    $('#txtOrderDate').attr('min',today);
+}
+
+
+/*$("#txtOrderDate").dataset({ minDate: 0});*/
 
 function loadAllCustomersForOption() {
     $("#selectCusID").empty();
